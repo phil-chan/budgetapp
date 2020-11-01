0fc2b9ba8f4f71f3880760cb6a5c5157eaec1078
Philip Chan <filnzl@hotmail.com>
2020-11-01 22:15:17 +1300

    update add form for formatting but currency input needs work

--- client/components/Add.jsx
+++ client/components/Add.jsx
@@ -1,40 +1,100 @@
 import React from "react";
 import { connect } from "react-redux";
-import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
-import { apiAddExpense } from '../apis/index'
+import { apiAddExpense } from "../apis/index";
 
 class Add extends React.Component {
   state = {
     date: Date.now(),
-    category: '',
-    expense_name: '',
-    expense_description: '',
-    cost: '',
-    user_id: this.props.auth.user.id
+    category: "",
+    expense_name: "",
+    expense_description: "",
+    cost: 0,
+    user_id: this.props.auth.user.id,
   };
 
   handleSubmit = (e) => {
-    e.preventDefault()
-    apiAddExpense(this.state)
-  }
+    e.preventDefault();
+    apiAddExpense(this.state);
+    this.props.history.push("/budgetapp");
+  };
 
   handleChange = (e) => {
     this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   render() {
     return (
       <>
-        <h1>Add</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input required className="input" placeholder="Expense Name" value={this.state.expense_name} name="expense_name" onChange={this.handleChange} />
-          <input required className="input" placeholder="Category" value={this.state.category} name="category" onChange={this.handleChange} />
-          <input required className="input" placeholder="Description" value={this.state.expense_description} name="expense_description" onChange={this.handleChange} />
-          <input required className="input" placeholder="Cost" value={this.state.cost} name="cost" onChange={this.handleChange} />
-          <button onClick={() => this.handleSubmit}>Submit</button>
+        <form className="form box" onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input
+                required
+                className="input is"
+                placeholder="i.e. McDonald's Cheese Burger"
+                value={this.state.expense_name}
+                name="expense_name"
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Category</label>
+            <div className="control">
+              <div class="select is-info">
+                <select required name="category" onChange={this.handleChange}>
+                  <option></option>
+                  <option value="Entertainment">Entertainment</option>
+                  <option value="Food">Food</option>
+                  <option value="Charity">Charity</option>
+                  <option value="Travel">Travel</option>
+                  <option value="Work">Work</option>
+                  <option value="Subscriptions">Subscriptions</option>
+                  <option value="Other">Other</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Description</label>
+            <div className="control">
+              <textarea
+                required
+                className="textarea"
+                placeholder="i.e. Lunch with friends"
+                value={this.state.expense_description}
+                name="expense_description"
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Cost</label>
+            <div className="control columns column is-one-quarter">
+              <input
+                required
+                className="input"
+                // value={`$${Number.parseFloat(this.state.cost).toFixed(2)}`}
+                value={this.state.cost}
+                name="cost"
+                type="number"
+                pattern="^\$\d{1,3}(,\d{3})*(\.\d+)?$"
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <button className="button is-info" onClick={() => this.handleSubmit}>
+            Submit
+          </button>
         </form>
       </>
     );
